eb9361a2712863a87ba4ca5afea0cdfde401aa49
zhb-324 <7763307+zhb32412@user.noreply.gitee.com>
2023-04-22 13:41:29 +0800

    Initial Commit

---
+++ MacOS-SwiftUI-StatusBar/MacOS_SwiftUI_StatusBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOS_SwiftUI_StatusBarApp.swift
+//  MacOS-SwiftUI-StatusBar
+//
+//  Created by bin zhu on 2023/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MacOS_SwiftUI_StatusBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacOS-SwiftUI-StatusBarUITests/MacOS_SwiftUI_StatusBarUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MacOS_SwiftUI_StatusBarUITestsLaunchTests.swift
+//  MacOS-SwiftUI-StatusBarUITests
+//
+//  Created by bin zhu on 2023/4/22.
+//
+
+import XCTest
+
+final class MacOS_SwiftUI_StatusBarUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
